99921bcde84809b176e8d5c5fdac65cb11011a36
marcio <marciobouhid@gmail.com>
2023-02-14 11:29:25 +0000

    initial commit

---
+++ Dashboad.py
@@ -0,0 +1,14 @@
+# Libraries
+import streamlit as st
+
+
+# Tab Title
+st.set_page_config(
+    page_title='Pallet Dashboad',
+    page_icon='🪵',
+    layout='wide')
+
+# Page Title
+st.title('DASHBOAD')
+
+
